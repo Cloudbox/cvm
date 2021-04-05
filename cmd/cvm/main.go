5ab5a193a081feed404d38ef78a7aa251b4e067c
James Bayliss <l3uddz@gmail.com>
2021-04-05 11:59:57 +0100

    refactor: default config directory function does not need to be public

--- cmd/cvm/main.go
+++ cmd/cvm/main.go
@@ -58,8 +58,8 @@ func main() {
 		}),
 		kong.Vars{
 			"version":     fmt.Sprintf("%s (%s@%s)", build.Version, build.GitCommit, build.Timestamp),
-			"config_file": filepath.Join(GetDefaultConfigDirectory("crs", "config.yml"), "config.yml"),
-			"log_file":    filepath.Join(GetDefaultConfigDirectory("crs", "config.yml"), "activity.log"),
+			"config_file": filepath.Join(defaultConfigDirectory("crs", "config.yml"), "config.yml"),
+			"log_file":    filepath.Join(defaultConfigDirectory("crs", "config.yml"), "activity.log"),
 		},
 	)
 
